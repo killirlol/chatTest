8174fc5a35389d9ef81f3160150526f016d315b2
killirlol <killirlol77@gmail.com>
2023-03-02 23:54:45 +0400

    j

---
+++ scripts/scriptChat.js
@@ -0,0 +1,13 @@
+let a = 56;
+const input = document.getElementById("send_msg");
+// const buttun = document.getElementById("send_btn");
+const message_area = document.getElementsByClassName("message_area")
+const button = document.getElementById("send_btn");
+
+button.addEventListener("click", function() {
+  console.log("Button clicked!");
+});
+
+// function press(){
+//     message_area.appChaild("dfghjk");
+// }
